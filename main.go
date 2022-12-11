9fde49992ed651b8a5b7a776c1190dd23448944b
Cihaan <cihankafadar1@gmail.com>
2022-12-11 14:39:03 +0300

    feat: get top consumer

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
+const FILE_NAME string = "input.txt"
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Print(getMaxConsumer())
+
+}
+
+func readFile(path string) (*bufio.Scanner, *os.File) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return bufio.NewScanner(file), file
+}
+
+func getMaxConsumer() int {
+	var res int = 0
+	var count int = 0
+	var scanner, file = readFile(FILE_NAME)
+
+	for scanner.Scan() {
+		var parsed, err = strconv.Atoi(scanner.Text())
+		count += parsed
+
+		if err != nil {
+			if res < count {
+				res = count
+				count = 0
+			} else {
+				count = 0
+			}
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	return res
 }
